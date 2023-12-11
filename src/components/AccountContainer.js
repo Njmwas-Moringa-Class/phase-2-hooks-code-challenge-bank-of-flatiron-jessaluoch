709f8e8748a3741784df33e85f6d61793b03ee66
jessaluoch <jessica.aluoch@student.moringaschool.com>
2023-12-11 18:54:02 +0300

    corrections

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+  const [transactions, setTransactions] = useState([]);
+  const [ search, setSearch] = useState(""); 
+  
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}`)
+      .then((r) => r.json())
+      .then((data) => setTransactions(data)) 
+  }, [])
+
+  function updatedTransactions(newTransactions) {
+   const updatedTransactionsArray = [...transactions, newTransactions]
+   setTransactions(updatedTransactionsArray)
+  }
+
   return (
     <div>
-      <Search />
-      <AddTransactionForm />
-      <TransactionsList />
+      <Search search={search} setSearch={setSearch}/>
+      <AddTransactionForm newData={updatedTransactions} />
+      <TransactionsList transactions={transactions} setTransactions={setTransactions} search={search} />
     </div>
   );
 }
+
 
 export default AccountContainer;
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,12 +1,27 @@
 import React from "react";
 
-function Transaction() {
+function Transaction({id, date, description, category, amount}) {
+  function handleDelete(event){
+    fetch(`${process.env.REACT_APP_API_URL}/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+    .then(result => result.json())
+    .then(alert("Item deleted!"))
+    
+
+  }
+
   return (
     <tr>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
+      <td>{date}</td>
+      <td>{description}</td>
+      <td>{category}</td>
+      <td>{amount}</td>
+      <td>{<button onClick={handleDelete}>Delete</button>}</td>
     </tr>
   );
 }
