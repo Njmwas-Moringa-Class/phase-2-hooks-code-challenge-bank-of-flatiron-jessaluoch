cb5e9feae4278ff03956f21547d21af606541243
jessaluoch <jessica.aluoch@student.moringaschool.com>
2023-12-13 14:47:13 +0300

    troubleshooting

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AccountContainer from "./AccountContainer";
 
 function App() {
+
+  const [transactions, setTransactions] = useState([]);
+
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      try {
+        const response = await fetch("http://localhost:8001/transactions");
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch transactions");
+        }
+
+        const data = await response.json();
+        setTransactions(data);
+      } catch (error) {
+        console.error("Error fetching transactions:", error);
+      }
+    };
+
+    fetchTransactions();
+  }, []); 
+
   return (
     <div className="ui raised segment">
       <div className="ui segment violet inverted">
         <h2>The Royal Bank of Flatiron</h2>
       </div>
-      <AccountContainer />
+      {/* Pass the transactions data to the AccountContainer component */}
+      <AccountContainer transactions={transactions} />
     </div>
   );
 }
